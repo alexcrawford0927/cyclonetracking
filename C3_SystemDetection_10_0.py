c3a2d12d376b31d8f0b979bc28ce417beed429be
alexcrawford0927 <alexcrawford0927@gmail.com>
2019-02-09 16:53:35 -0500

    Update to C3_SystemDetection_10_0.py
    Changed which lines are commented or uncommented to better reflect the most likely use.

--- C3_SystemDetection_10_0.py
+++ C3_SystemDetection_10_0.py
@@ -24,73 +24,73 @@ import pandas as pd
 import cPickle
 import numpy as np
 import CycloneModule_10_3 as md
-#
-#'''*******************************************
-#Set up Environment
-#*******************************************'''
-#print "Setting up environment."
-#BBox = "" # use "" if performing on all cyclones
-#
-#path = "F:/GISDATA"
-#inpath = path+"/ArcticCyclone/detection10_10E/"+BBox
-#outpath = inpath
-#
-#'''*******************************************
-#Define Variables
-#*******************************************'''
-#print "Defining variables"
-## Regenesis Paramater
-#rg = 1 
-## 0 = regenesis starts a new system track; 
-## 1 = regenesis continues preivous system track with new ptid
-#
-## Time Variables
-#time1 = [2016,8,1,0,0,0] # Format: [Y,M,D,H,M,S]
-#time2 = [2016,9,1,0,0,0] # stop BEFORE this time (exclusive)
-#monthstep = [0,1,0,0,0,0] # A Time step that increases by 1 mont [Y,M,D,H,M,S]
-#
-#dateref = [1900,1,1,0,0,0] # [Y,M,D,H,M,S]
-#starttime = [1979,1,1,0,0,0] # [Y,M,D,H,M,S]... starttime for the cyclone detection script
-#
-#months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
-#mons = ["01","02","03","04","05","06","07","08","09","10","11","12"]
-#
-#'''*******************************************
-#Main Analysis
-#*******************************************'''
-#print "Main Analysis"
-#
-#mt = time1
-#while mt != time2:
-#    # Extract date
-#    Y = str(mt[0])
-#    M = mons[mt[1]-1]
-#    print " " + Y + " - " + M
-#    
-#    # Load Cyclone Tracks
-#    ct = md.unpickle(inpath+"/CycloneTracks/"+Y+"/"+BBox+"cyclonetracks"+Y+M+".pkl")
-#    
-#    # Create System Tracks
-#    if mt == starttime:
-#        cs, cs0 = md.cTrack2sTrack(ct,[],dateref,rg)
-#        md.pickle(cs,inpath+"/SystemTracks/"+Y+"/"+BBox+"systemtracks"+Y+M+".pkl")
-#    
-#    else:
-#        # Extract date for previous month
-#        mt0 = md.timeAdd(mt,[-d for d in monthstep])
-#        Y0 = str(mt0[0])
-#        M0 = mons[mt0[1]-1]
-#        
-#        # Load previous month's system tracks
-#        cs0 = md.unpickle(inpath+"/SystemTracks/"+Y0+"/"+BBox+"systemtracks"+Y0+M0+".pkl")
-#        
-#        # Create system tracks
-#        cs, cs0 = md.cTrack2sTrack(ct,cs0,dateref,rg)
-#        md.pickle(cs,inpath+"/SystemTracks/"+Y+"/"+BBox+"systemtracks"+Y+M+".pkl")
-#        md.pickle(cs0,inpath+"/SystemTracks/"+Y0+"/"+BBox+"systemtracks"+Y0+M0+".pkl")
-#    
-#    # Increment Time Step
-#    mt = md.timeAdd(mt,monthstep)
-#
-#print 'Elapsed time:',round(clock()-start,2),'seconds'
-#print "Complete."
+
+'''*******************************************
+Set up Environment
+*******************************************'''
+print "Setting up environment."
+BBox = "" # use "" if performing on all cyclones
+
+path = "F:/GISDATA"
+inpath = path+"/ArcticCyclone/detection10_10E/"+BBox
+outpath = inpath
+
+'''*******************************************
+Define Variables
+*******************************************'''
+print "Defining variables"
+# Regenesis Paramater
+rg = 1 
+# 0 = regenesis starts a new system track; 
+# 1 = regenesis continues preivous system track with new ptid
+
+# Time Variables
+time1 = [2016,8,1,0,0,0] # Format: [Y,M,D,H,M,S]
+time2 = [2016,9,1,0,0,0] # stop BEFORE this time (exclusive)
+monthstep = [0,1,0,0,0,0] # A Time step that increases by 1 mont [Y,M,D,H,M,S]
+
+dateref = [1900,1,1,0,0,0] # [Y,M,D,H,M,S]
+starttime = [1979,1,1,0,0,0] # [Y,M,D,H,M,S]... starttime for the cyclone detection script
+
+months = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+mons = ["01","02","03","04","05","06","07","08","09","10","11","12"]
+
+'''*******************************************
+Main Analysis
+*******************************************'''
+print "Main Analysis"
+
+mt = time1
+while mt != time2:
+    # Extract date
+    Y = str(mt[0])
+    M = mons[mt[1]-1]
+    print " " + Y + " - " + M
+    
+    # Load Cyclone Tracks
+    ct = md.unpickle(inpath+"/CycloneTracks/"+Y+"/"+BBox+"cyclonetracks"+Y+M+".pkl")
+    
+    # Create System Tracks
+    if mt == starttime:
+        cs, cs0 = md.cTrack2sTrack(ct,[],dateref,rg)
+        md.pickle(cs,inpath+"/SystemTracks/"+Y+"/"+BBox+"systemtracks"+Y+M+".pkl")
+    
+    else:
+        # Extract date for previous month
+        mt0 = md.timeAdd(mt,[-d for d in monthstep])
+        Y0 = str(mt0[0])
+        M0 = mons[mt0[1]-1]
+        
+        # Load previous month's system tracks
+        cs0 = md.unpickle(inpath+"/SystemTracks/"+Y0+"/"+BBox+"systemtracks"+Y0+M0+".pkl")
+        
+        # Create system tracks
+        cs, cs0 = md.cTrack2sTrack(ct,cs0,dateref,rg)
+        md.pickle(cs,inpath+"/SystemTracks/"+Y+"/"+BBox+"systemtracks"+Y+M+".pkl")
+        md.pickle(cs0,inpath+"/SystemTracks/"+Y0+"/"+BBox+"systemtracks"+Y0+M0+".pkl")
+    
+    # Increment Time Step
+    mt = md.timeAdd(mt,monthstep)
+
+print 'Elapsed time:',round(clock()-start,2),'seconds'
+print "Complete."
