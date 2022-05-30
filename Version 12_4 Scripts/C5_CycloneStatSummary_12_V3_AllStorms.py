75fef4d14cc6d15b76f6722eede4c9467688d0b4
Alex Crawford <alexcrawford0927@gmail.com>
2022-05-30 12:35:16 -0500

    New version of Cyclone Stat Summary
    C5_CycloneStatSummary_12_V3_AllStorms.py had a bug in it that only affected attempts to use with "cyclone" tracks. The more commonly used "system" tracks were unaffected. Also edited the pandas data frame creation to be marginally more efficient.

--- Version 12_4 Scripts/C5_CycloneStatSummary_12_V3_AllStorms.py
+++ Version 12_4 Scripts/C5_CycloneStatSummary_12_V3_AllStorms.py
@@ -29,11 +29,11 @@ def maxDistFromGenPnt(data):
 '''*******************************************
 Set up Environment
 *******************************************'''
-BBoxNum = "BBox13" # Use "BBox##" or "" if no subset
+BBoxNum = "" # Use "BBox##" or "" if no subset
 path = "/Volumes/Cressida"
-version = "12_9E5R"
+version = "12_4TestTracks"
 inpath = path+"/CycloneTracking/tracking"+version+"/"+BBoxNum
-regpath = path+"/Projections/EASE2_N0_25km_GenesisRegions.nc"
+regpath = path+"/Projections/EASE2_N0_100km_GenesisRegions.nc"
 
 '''*******************************************
 Define Variables
@@ -50,12 +50,12 @@ V = "_GenReg" # An optional version name; suggested to start with "_" or "-" to 
 # Aggregation Parameters
 minls = 1 # minimum lifespan (in  days) for a track to be considered
 mintl = 1000 # minimum track length (in km)
-mindisp = 0
+mindisp = 0 # the maximum displacement from the genesis point must be at least this large (in km)
 minlat = 0 # minimum latitude
 
 # Time Variables 
 starttime = [1979,1,1,0,0,0] # Format: [Y,M,D,H,M,S]
-endtime = [2020,1,1,0,0,0] # stop BEFORE this time (exclusive)
+endtime = [1979,2,1,0,0,0] # stop BEFORE this time (exclusive)
 monthstep = [0,1,0,0,0,0] # A Time step that increases by 1 mont [Y,M,D,H,M,S]
 
 dateref = [1900,1,1,0,0,0] # [Y,M,D,H,M,S]
@@ -70,8 +70,9 @@ print("Main Analysis")
 # Load Regions
 regs = nc.Dataset(regpath)['reg'][:].data
 
-# Prep empty pdf
-pdf = pd.DataFrame()
+# Create Empty lists
+sid, tid, year, month, avguv, maxdsqp, minp, maxdepth, avgdsqp, avgp, avgdepth = [[] for i in range(11)]
+lifespan, trlen, avgarea, mcc, spl1, spl2, spl3, mrg1, mrg2, mrg3, rge2, genReg, lysReg = [[] for i in range(13)]
 
 mt = starttime
 while mt != endtime:
@@ -87,7 +88,7 @@ while mt != endtime:
     
     # Load tracks
     cs = pd.read_pickle(inpath+"/"+kind+"/"+Y+"/"+BBoxNum+kind.lower()+Y+M+".pkl") # Current Month
-    try:
+    try: # Previous Month
         cs0 = pd.read_pickle(inpath+"/"+kind+"/"+str(mt0[0])+"/"+BBoxNum+kind.lower()+str(mt0[0])+mons[mt0[1]-1]+".pkl")
     except:
         cs0 = []
@@ -158,18 +159,40 @@ while mt != endtime:
                     elif tr.events.Etype.iloc[e] == 3:
                         spl3_count =  spl3_count + 3
         
-        row = pd.DataFrame([{"sid":tr.sid,"year":mt[0],"month":mt[1],"avguv":tr.data.uv.mean(),\
-        "maxdsqp":tr.data.DsqP.max(),"minp":tr.data.p_cent.min(),"maxdepth":tr.data.depth.max(),\
-        "avgdsqp":tr.data.DsqP.mean(),"avgp":tr.data.p_cent.mean(),"avgdepth":tr.data.depth.mean(),\
-        "lifespan":tr.lifespan(),"trlen":tr.trackLength(),"avgarea":tr.avgArea(),\
-        "mcc":tr.mcc(),"spl1":spl1_count,"spl2":spl2_count,"spl3":spl3_count,\
-        "mrg1":mrg1_count,"mrg2":mrg2_count,"mrg3":mrg3_count,"rge2":rge2_count,\
-        "genReg":regs[list(tr.data.loc[tr.data.type > 0,'y'])[0],list(tr.data.loc[tr.data.type > 0,'x'])[0]],\
-        "lysReg":regs[list(tr.data.y)[-1],list(tr.data.x)[-1]]},])
-        pdf = pdf.append(row,ignore_index=True,sort=False)
+        
+        # Append to lists
+        try:
+            sid.append(tr.sid)
+        except:
+            tid.append(tr.tid)
+            
+        year.append(mt[0]), month.append(mt[1]), avguv.append( tr.data.uv.mean() )
+        maxdsqp.append( tr.data.DsqP.max() ), minp.append( tr.data.p_cent.min() )
+        maxdepth.append( tr.data.depth.max() ), avgdsqp.append( tr.data.DsqP.mean() )
+        avgp.append( tr.data.p_cent.mean() ), avgdepth.append( tr.data.depth.mean() )
+        lifespan.append( tr.lifespan() ), trlen.append( tr.trackLength() )
+        avgarea.append( tr.avgArea() ), mcc.append( tr.mcc() )
+        spl1.append( spl1_count ), spl2.append( spl2_count ), spl3.append( spl3_count )
+        mrg1.append( mrg1_count ), mrg2.append( mrg2_count ), mrg3.append( mrg3_count ), rge2.append( rge2_count )
+        genReg.append( regs[list(tr.data.loc[tr.data.type > 0,'y'])[0],list(tr.data.loc[tr.data.type > 0,'x'])[0]] )
+        lysReg.append( regs[list(tr.data.y)[-1],list(tr.data.x)[-1]] )
     
     mt = md.timeAdd(mt,monthstep)
 
+# Construct Pandas Dataframe
+if kind1 == 'System':
+    pdf = pd.DataFrame({"sid":sid,"year":year,"month":month,"avguv":avguv,\
+    "maxdsqp":maxdsqp,"minp":minp,"maxdepth":maxdepth,"avgdsqp":avgdsqp,\
+    "avgp":avgp,"avgdepth":avgdepth,"lifespan":lifespan,"trlen":trlen,\
+    "avgarea":avgarea,"mcc":mcc,"spl1":spl1,"spl2":spl2,"spl3":spl3,\
+    "mrg1":mrg1,"mrg2":mrg2,"mrg3":mrg3,"rge2":rge2,"genReg":genReg,"lysReg":lysReg})
+else:
+    pdf = pd.DataFrame({"tid":tid,"year":year,"month":month,"avguv":avguv,\
+    "maxdsqp":maxdsqp,"minp":minp,"maxdepth":maxdepth,"avgdsqp":avgdsqp,\
+    "avgp":avgp,"avgdepth":avgdepth,"lifespan":lifespan,"trlen":trlen,\
+    "avgarea":avgarea,"mcc":mcc,"spl1":spl1,"spl2":spl2,"spl3":spl3,\
+    "mrg1":mrg1,"mrg2":mrg2,"mrg3":mrg3,"rge2":rge2,"genReg":genReg,"lysReg":lysReg}) 
+        
 # Append columns for overall merge, split, non-interacting
 pdf["mrg"] = np.array((pdf["mrg2"] >= 1) | (pdf["mrg3"] >= 1)).astype(int)
 pdf["spl"] = np.array((pdf["spl2"] >= 1) | (pdf["spl3"] >= 1)).astype(int)
